04c80941c0f66d65d480d36f6c98e6bb5185d415
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-09-25 14:26:53 +0300

    hooked up the search filters to the URL and created firestore indexes

--- app/property-search/filters-form.tsx
+++ app/property-search/filters-form.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import z from 'zod';
 
@@ -14,17 +15,36 @@ const formSchema = z.object({
 });
 
 export default function FiltersForm() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      maxPrice: '',
-      minBedrooms: '',
-      minPrice: '',
+      maxPrice: searchParams.get('maxPrice') ?? '',
+      minBedrooms: searchParams.get('minBedrooms') ?? '',
+      minPrice: searchParams.get('minPrice') ?? '',
     },
   });
 
   const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-    console.log({ data });
+    // console.log({ data });
+    const newSearchParams = new URLSearchParams();
+
+    if (data.minPrice) {
+      newSearchParams.set('minPrice', data.minPrice);
+    }
+
+    if (data.maxPrice) {
+      newSearchParams.set('maxPrice', data.maxPrice);
+    }
+
+    if (data.minBedrooms) {
+      newSearchParams.set('minBedrooms', data.minBedrooms);
+    }
+
+    newSearchParams.set('page', '1');
+    router.push(`/property-search?${newSearchParams.toString()}`);
   };
 
   return (
--- app/property-search/page.tsx
+++ app/property-search/page.tsx
@@ -1,7 +1,40 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import FiltersForm from './filters-form';
+import { Suspense } from 'react';
+import { getProperties } from '@/data/properties';
 
-export default function PropertySearch() {
+export default async function PropertySearch({
+  searchParams,
+}: {
+  searchParams: Promise<any>;
+}) {
+  const searchParamsValue = await searchParams;
+
+  const parsedPage = parseInt(searchParamsValue?.page);
+  const parsedMinPrice = parseInt(searchParamsValue?.minPrice);
+  const parsedMaxPrice = parseInt(searchParamsValue?.maxPrice);
+  const parsedMinBedrooms = parseInt(searchParamsValue?.minBedrooms);
+
+  const page = isNaN(parsedPage) ? 1 : parsedPage;
+  const minPrice = isNaN(parsedMinPrice) ? null : parsedMinPrice;
+  const maxPrice = isNaN(parsedMaxPrice) ? null : parsedMaxPrice;
+  const minBedrooms = isNaN(parsedMinBedrooms) ? null : parsedMinBedrooms;
+
+  const properties = await getProperties({
+    pagination: {
+      page,
+      pageSize: 3,
+    },
+    filters: {
+      minPrice,
+      maxPrice,
+      minBedrooms,
+      status: ['for-sale'],
+    },
+  });
+
+  console.log({ properties });
+
   return (
     <div className="max-w-screen-lg mx-auto">
       <h1 className="text-4xl font-bold p-5">Property Search</h1>
@@ -10,7 +43,9 @@ export default function PropertySearch() {
           <CardTitle>Filters</CardTitle>
         </CardHeader>
         <CardContent>
-          <FiltersForm />
+          <Suspense>
+            <FiltersForm />
+          </Suspense>
         </CardContent>
       </Card>
     </div>
