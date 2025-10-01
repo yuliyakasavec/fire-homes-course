e67ef6416bca7780f8ef97d3b503ca48954c0c00
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-10-01 10:38:18 +0300

    rendered the login form in the login modal

--- app/(auth)/login/login-form.tsx
+++ app/(auth)/login/login-form.tsx
@@ -1,101 +1,16 @@
 'use client';
 
-import ContinueWithGoogleButton from '@/components/continue-with-google-button';
-import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { useAuth } from '@/context/auth';
-import { passwordValidation } from '@/validation/registerUser';
-import { zodResolver } from '@hookform/resolvers/zod';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useForm } from 'react-hook-form';
-import { toast } from 'sonner';
-import { z } from 'zod';
+import CommonLoginForm from '@/components/login-form';
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: passwordValidation,
-});
-
-export default function LoginForm({ onSuccess }: { onSuccess?: () => void }) {
+export default function LoginForm() {
   const router = useRouter();
-  const auth = useAuth();
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
-  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
-    try {
-      await auth?.loginWithEmail(data.email, data.password);
-      onSuccess?.();
-      router.refresh();
-    } catch (e: any) {
-      toast.error('Error!', {
-        description:
-          e.code === 'auth/invalid-credential'
-            ? 'Incorrect credentials'
-            : 'An error occurred',
-      });
-    }
-  };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <fieldset
-          disabled={form.formState.isSubmitting}
-          className="flex flex-col gap-4"
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormControl>
-                    <Input {...field} placeholder="Email" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormControl>
-                    <Input {...field} placeholder="Password" type="password" />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <Button type="submit">Login</Button>
-          <div>
-            Forgotten your password?
-            <Link href="/forgot-password" className="pl-2 underline">
-              Reset it here.
-            </Link>
-          </div>
-          <div className="text-center pb-5">or</div>
-        </fieldset>
-      </form>
-      <ContinueWithGoogleButton />
-    </Form>
+    <CommonLoginForm
+      onSuccess={() => {
+        router.refresh();
+      }}
+    />
   );
 }
--- app/property-search/@modal/(..)login/page.tsx
+++ app/property-search/@modal/(..)login/page.tsx
@@ -1,3 +1,49 @@
+'use client';
+
+import LoginForm from '@/components/login-form';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { loginSuccess } from './actions';
+
 export default function LoginModal() {
-  return <div>login modal</div>;
+  const router = useRouter();
+  return (
+    <Dialog
+      open
+      onOpenChange={() => {
+        router.back();
+      }}
+    >
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Login</DialogTitle>
+          <DialogDescription>
+            You must be logged in to favourite a property
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <LoginForm
+            onSuccess={async () => {
+              await loginSuccess();
+              router.back();
+            }}
+          />
+        </div>
+        <DialogFooter className="block">
+          Don&apos;t have an account?
+          <Link className="underline pl-2" href="/register">
+            Register here
+          </Link>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
