f4fa7005d4664617221d4332cdf919753528b315
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-10-02 15:11:40 +0300

    implemented delete account functionality

---
+++ app/account/delete-account-button.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { removeToken } from '@/context/actions';
+import { useAuth } from '@/context/auth';
+import {
+  deleteUser,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+} from 'firebase/auth';
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+export default function DeleteAccountButton() {
+  const auth = useAuth();
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const handleDeleteClick = async () => {
+    if (auth?.currentUser?.email) {
+      setIsDeleting(true);
+      try {
+        await reauthenticateWithCredential(
+          auth.currentUser,
+          EmailAuthProvider.credential(auth.currentUser.email, password)
+        );
+        await deleteUser(auth.currentUser);
+        await removeToken();
+
+        toast.success('Success!', {
+          description: 'Your account was deleted successfully!',
+        });
+      } catch (e) {
+        toast.error('Error!', {
+          description:
+            e.code === 'auth/invalid-credential'
+              ? 'Your current password is incorrect'
+              : 'an error occurred',
+        });
+        setIsDeleting(false);
+        setPassword('');
+      }
+    }
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className="w-full">
+          Delete Account
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to delete your account?
+          </AlertDialogTitle>
+          <AlertDialogDescription asChild>
+            <div>
+              This action cannot be undone. This will permanently delete your
+              account and remove all your data from our servers.
+              <div>
+                <Label className="pt-2 pb-4">
+                  Enter current password to continue
+                </Label>
+                <Input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                />
+              </div>
+            </div>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDeleteClick} disabled={isDeleting}>
+            {isDeleting ? 'Deleting...' : 'Delete Account'}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,10 +1,17 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Label } from '@/components/ui/label';
 import { auth } from '@/firebase/server';
 import { DecodedIdToken } from 'firebase-admin/auth';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import UpdatePasswordForm from './update-password-form';
+import DeleteAccountButton from './delete-account-button';
 
 export default async function Account() {
   const cookieStore = await cookies();
@@ -37,6 +44,14 @@ export default async function Account() {
           <div>{decodedToken.email}</div>
           {!!isPasswordProvider && <UpdatePasswordForm />}
         </CardContent>
+        {!decodedToken.admin && (
+          <CardFooter className="flex flex-col items-start">
+            <h2 className="text-red-500 text-2xl font-bold mb-2">
+              Danger Zone
+            </h2>
+            <DeleteAccountButton />
+          </CardFooter>
+        )}
       </Card>
     </div>
   );
